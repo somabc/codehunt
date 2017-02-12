c573045b38a520ed378d85ce86e362ff472349ca
somabc <somabc@gmail.com>
2017-02-12 22:00:29 +0000

    popup handler

--- src/react-components/Navbar/PostPopup.js
+++ src/react-components/Navbar/PostPopup.js
@@ -2,9 +2,41 @@ import React from 'react';
 import Popup from './Popup';
 
 class PostPopup extends React.Component {
-    render () {
-      return (
-        
-      )
-    }
+  handlePost = () => {
+
+  };
+  
+  render() {
+    return (
+      <Popup {...this.props} style="post-popup">
+        <header>Post a new product></header>
+        <section>
+          <table>
+            <tbody>
+              <tr>
+                <td>Name</td>
+                <td><input placeholder="Enter product's name"></td>
+              </tr>
+              <tr>
+                <td>Descritpion</td>
+                <td><input placeholder="Enter product's description"></td>
+              </tr>
+              <tr>
+                <td>Link</td>
+                <td><input placeholder="http://www..."></td>
+              </tr>
+              <tr>
+                <td>Media</td>
+                <td><input placeholder="Paste a direct link to an image"></td>
+              </tr>
+            </tbody>
+          </section>
+            <footer className="post-footer">
+              <button onClick={this.handlePost} className="post-btn">Hunt It!</button>
+            </footer>
+      </Popup>
+    );
+  }
 }
+
+export default PostPopup;
