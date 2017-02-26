ff809d04fd66ee3c8a1275ffba3e77d5cb7bf7be
somabc <somabc@gmail.com>
2017-02-26 16:29:51 +0000

    handle popup menu

--- src/react-components/Navbar/ProfileMenu.js
+++ src/react-components/Navbar/ProfileMenu.js
@@ -1,6 +1,35 @@
 import React from 'react';
 
 class ProfileMenu extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showProfileNav: false
+    }
+  }
+
+  handleClick = () => {
+    if (this.state.showProfileNav) {
+      this.setState({showProfileNav: false});
+    } else {
+      this.setState({showProfileNav: true});
+    }
+  };
+
+  handleClickOutsite = (e) => {
+    if (e.target != this.refs.profileBtn) {
+      this.setState({showProfileNav: false});
+    }
+  };
+
+  componentWillMount() {
+    window.addEventListener("click", this.handleClickOutsite, false);
+  }
+
+  componentWillUnMount() {
+    window.addEventListener("click", this.handleClickOutsite, false);
+  }
+
   renderProfileNav() {
     return (
       <nav classname="profile-nav" ref="profileNav">
@@ -14,8 +43,11 @@ class ProfileMenu extends React.Component {
   render(){
       return (
         <section className="profile-menu">
-          <img src="/img/leo.jpeg" className="profile-btn medium-avatar"/>
-          {this.renderProfileNav()}
+          <img src="/img/leo.jpeg" onClick={this.handleClick} className="profile-btn medium-avatar" ref="profileBtn"/>
+          {
+            this.state.showProfileNav? this.renderProfileNav() : null
+          }
+
         </section>
       );
   }
